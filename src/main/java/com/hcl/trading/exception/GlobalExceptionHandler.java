6f2f70248b6671bfaf6440d73d8fac58ff2de9f1
bindubogarapu <bogarapu.bindu@gmail.com>
2019-08-26 17:30:56 +0530

    card updation added

--- src/main/java/com/hcl/trading/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hcl/trading/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.hcl.trading.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.hcl.trading.dto.ResponseDto;
@@ -20,13 +24,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	}
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ResponseDto> globalExceptionHandler(Exception exception, WebRequest request) {
-		ResponseDto responseDto = new ResponseDto();
-		responseDto.setMessage(exception.getMessage());
-		responseDto.setStatusCode(401);
-		return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
-
-	}
 
 }
